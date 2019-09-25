14c289d34ab20c6e991fcc7d9f4634bf19bc0d82
ryselis <karolis.ryselis@ktu.edu>
2019-09-25 19:38:52 +0300

    updated code example with comments

--- 06-Užduotys/async_chain/src/main/java/edu/ktu/ryselis/OpenWeatherAPI.java
+++ 06-Užduotys/async_chain/src/main/java/edu/ktu/ryselis/OpenWeatherAPI.java
@@ -15,10 +15,14 @@ import java.nio.file.Paths;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
+/**
+ * A class that encapsulates communication with OpenWeather service
+ */
 class OpenWeatherAPI {
     private String apiKey;
 
     OpenWeatherAPI() {
+        // sets API key from api_key.txt file. One should register for the key at OpenWeather web page
         try {
             byte[] encoded = Files.readAllBytes(Paths.get("api_key.txt"));
             apiKey = new String(encoded, Charset.defaultCharset());
@@ -27,18 +31,35 @@ class OpenWeatherAPI {
         }
     }
 
+    /**
+     * Makes a request to download current temperature for a city
+     * @param cityId - ID of the city to get temperature for
+     * @return JSON returned by OpenWeather
+     * @throws IOException if failed to connect to OpenWeather service
+     */
     private String makeOpenWeatherRequest(int cityId) throws IOException {
+        // url should look like this: http://api.openweathermap.org/data/2.5/weather?id=<cityId>&appid=<apiKey>
         String apiRoot = "http://api.openweathermap.org/";
         URL url = new URL(apiRoot + "data/2.5/weather?id=" + cityId + "&appid=" + apiKey);
+        // make HTTP request
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
+        // get response to our request, it should be in JSON format and may consist of multiple lines
         BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        // get all lines and join them into a single string
         String content = reader.lines().collect(Collectors.joining());
+        // dispose open resources
         reader.close();
         conn.disconnect();
         return content;
     }
 
+    /**
+     * Given the full JSON response from OpenWeather API, extract temperature as a double
+     * @param jsonResponse - full response retrieved from OpenWeather API
+     * @return current temperature
+     * @throws ParseException if JSON passed into the method was invalid
+     */
     private double parseResponse(String jsonResponse) throws ParseException {
         JSONParser parser = new JSONParser();
         JSONObject object = (JSONObject) parser.parse(jsonResponse);
@@ -46,23 +67,34 @@ class OpenWeatherAPI {
         return (double) main.get("temp");
     }
 
+    /**
+     * Convert kelvins to degrees Celsius
+     * @param kelvins - temperature in kelvins
+     * @return temperature in Celsius
+     */
     private double kelvinToCelsius(double kelvins) {
         return kelvins - 273.15;
     }
 
+    /**
+     * Given city ID, return a future that will resolve to current temperature in that city
+     * @param cityId - ID of the city
+     * @return - future containing current temperature in the city
+     */
     CompletableFuture<Double> getTemperatureAsync(int cityId){
-        return CompletableFuture.supplyAsync(() -> {
+        // result is retrieved by making request to OpenWeather, parsing response and converting temperature to Celsius
+        return CompletableFuture.supplyAsync(() -> {  // start of future chain
             try {
-                return this.makeOpenWeatherRequest(cityId);
+                return this.makeOpenWeatherRequest(cityId);  // step 1. make request and get response
             } catch (IOException e) {
                 return null;
             }
         }).thenApply(res -> {
             try {
-                return this.parseResponse(res);
+                return this.parseResponse(res);  // step 2. get response and parse temperature
             } catch (ParseException e) {
                 return null;
             }
-        }).thenApply(this::kelvinToCelsius);
+        }).thenApply(this::kelvinToCelsius);  // step 3. get temperature in kelvins and convert it to Celsius
     }
 }
