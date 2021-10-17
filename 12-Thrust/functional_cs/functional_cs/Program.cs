8616c52cd205a775656be0f2967d9e8fa3287430
ryselis <karolis.ryselis@ktu.edu>
2021-10-17 14:20:31 +0300

    added functional csharp example

---
+++ 12-Thrust/functional_cs/functional_cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace functional_cs
+{
+    internal static class Program
+    {
+        private static IEnumerable<int> GetVector() => Enumerable.Range(0, 20);
+
+        private static void Main()
+        {
+            var vector = GetVector();
+            var sum = vector
+                .Where(x => x % 2 == 0)
+                .Select(x => x * x)
+                .Aggregate((acc, x) => acc + x);
+            Console.WriteLine(sum);
+        }
+    }
+}
