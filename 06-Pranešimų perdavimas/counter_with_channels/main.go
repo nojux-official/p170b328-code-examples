0db80074e1f19c7c2b44234d9176ed8811812ab0
ryselis <karolis.ryselis@ktu.edu>
2023-10-08 15:18:30 +0300

    added comments

--- 06-Pranešimų perdavimas/counter_with_channels/main.go
+++ 06-Pranešimų perdavimas/counter_with_channels/main.go
@@ -4,54 +4,59 @@ import (
 	"fmt"
 )
 
+const INCREASE = 0
+const DECREASE = 1
+const FINISHED = 2
+
+// the program spawns two types of threads - some of them send messages to increase the value of the counter, others -
+// to decrease the value. The count is managed by main - main is the receiver of the messages, other threads are the senders
 func main() {
+	// the amounts of threads
 	var increaserThreadCount = 10
 	var decreaserThreadCount = 9
-	var counter = SimpleUnboundedCounter{count: 0}
+	// the count that will be managed by main
+	var count = 0
+	// create a channel to exchange messages
 	var channel = make(chan int)
+	// start goroutines
 	for i := 0; i < increaserThreadCount; i++ {
 		go increase(channel)
 	}
 	for i := 0; i < decreaserThreadCount; i++ {
 		go decrease(channel)
 	}
+	// the threads can send three types of messages - to increase the value, to decrease the value or an end-of-work
+	// signal. When the amount of received end-of-work signals is equal to the amount of spawned threads, stop receiving
+	// messages
 	var receivedFinishSignals = 0
 	for receivedFinishSignals < increaserThreadCount+decreaserThreadCount {
+		// get the message and do the required operation based on the message value
 		var message = <-channel
 		switch message {
-		case 0: // increase value
-			counter.Increase()
-		case 1: // decrease value
-			counter.Decrease()
-		case 2: //finish
+		case INCREASE: // increase value
+			count++
+		case DECREASE: // decrease value
+			count--
+		case FINISHED: //finish
 			receivedFinishSignals++
 		}
 	}
-	fmt.Println(counter.count)
+	// print the final result
+	fmt.Println(count)
 }
 
+// increase - send 50 messages INCREASE to the channel
 func increase(channel chan<- int) {
 	for i := 0; i < 50; i++ {
-		channel <- 0
+		channel <- INCREASE
 	}
-	channel <- 2
+	channel <- FINISHED
 }
 
-func decrease(channel chan<- int){
+// decrease - send 50 messages DECREASE to the channel
+func decrease(channel chan<- int) {
 	for i := 0; i < 50; i++ {
-		channel <- 1
+		channel <- DECREASE
 	}
-	channel <- 2
-}
-
-type SimpleUnboundedCounter struct {
-	count int32
-}
-
-func (counter *SimpleUnboundedCounter) Increase() {
-	counter.count++
-}
-
-func (counter *SimpleUnboundedCounter) Decrease() {
-	counter.count--
+	channel <- FINISHED
 }
