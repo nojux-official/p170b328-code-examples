60d58b14291eaad6a6a233643f207e34115b07b3
ryselis <karolis.ryselis@ktu.edu>
2024-09-08 12:18:15 +0300

    fixed folder titles, improved C++ 20 support

--- 02-Procesai-gijos/simple_program_cpp/main.cpp
+++ 02-Procesai-gijos/simple_program_cpp/main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 #include <functional>
+#include <algorithm>
+#include <ranges>
 
 using namespace std;
 
@@ -13,12 +15,11 @@ void execute(const string &name);
 int main() {
     // create names for threads
     vector<string> names = {"First", "Second"};
-    // create a vector that will hold threads
-    vector<thread> threads(names.size());
-    // for each name create a thread and write to 'threads' vector. Created thread is immediately launched
-    transform(names.begin(), names.end(), threads.begin(), [](auto &name) {
-        return thread(execute, name);
-    });
+    // create a view that will hold threads - one thread per name
+    auto threads = names
+                   | views::transform([](auto& name) {
+                       return thread(execute, name);
+                   });
     // wait for each thread to finish
     for_each(threads.begin(), threads.end(), mem_fn(&thread::join));
     cout << "Program finished execution" << endl;
